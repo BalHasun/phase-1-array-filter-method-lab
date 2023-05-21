267920d0124a9588922afea4b2e0e2837b1c9678
Bal Elsada Hasun <fouadhason@gmail.com>
2023-05-21 10:38:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  const driverNames = drivers.filter((item) => {
+    return item === name || item === name.toLowerCase();
+  });
+  return driverNames ? driverNames : [];
+}
+function fuzzyMatch(drivers, name) {
+  const driverNames = drivers.filter((item) => {
+    return item.slice(0,2) === name ;
+  });
+  return driverNames ? driverNames : [];
+}
+
+function matchName(drivers, name) {
+  const driverNames = drivers.filter((item) => {
+    return item.name === name ;
+  });
+  return driverNames ? driverNames : [];
+}
